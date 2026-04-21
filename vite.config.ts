de427b6523b051a543cb18bf161960aa55be74a4
VEGA Intellisoft <vegaintellisoft.dev@gmail.com>
2026-04-21 15:44:28 +0530

    vite config

--- vite.config.ts
+++ vite.config.ts
@@ -1,35 +1,22 @@
-import { defineConfig, loadEnv } from 'vite'
+import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
-import basicSsl from '@vitejs/plugin-basic-ssl'
 import path from 'path'
 
-// https://vite.dev/config/
-export default defineConfig(({ mode }) => {
-  const env = loadEnv(mode, process.cwd(), '');
-  
-  return {
-    plugins: [
-      basicSsl(),
-      react({
-        babel: {
-          plugins: ['babel-plugin-react-compiler'],
-        },
-      })
-    ],
-    resolve: {
-      alias: {
-        '@': path.resolve(__dirname, './src'),
+export default defineConfig({
+  plugins: [
+    react({
+      babel: {
+        plugins: ['babel-plugin-react-compiler'],
       },
+    }),
+  ],
+  resolve: {
+    alias: {
+      '@': path.resolve(__dirname, './src'),
     },
-    server: {
-      port: 5173,
-      host: true,
-      proxy: {
-        '/api': {
-          target: env.VITE_API_URL,
-          changeOrigin: true,
-        },
-      },
-    },
-  };
+  },
+  server: {
+    port: 5173,
+    host: true,
+  },
 })
