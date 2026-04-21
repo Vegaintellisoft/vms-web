c8700657c9674fea18f9fc65f240d8a500dbc84c
VEGA Intellisoft <vegaintellisoft.dev@gmail.com>
2026-04-21 13:02:23 +0530

    fix

--- vite.config.ts
+++ vite.config.ts
@@ -1,31 +1,35 @@
-import { defineConfig } from 'vite'
+import { defineConfig, loadEnv } from 'vite'
 import react from '@vitejs/plugin-react'
 import basicSsl from '@vitejs/plugin-basic-ssl'
 import path from 'path'
 
 // https://vite.dev/config/
-export default defineConfig({
-  plugins: [
-    basicSsl(),
-    react({
-      babel: {
-        plugins: ['babel-plugin-react-compiler'],
-      },
-    })
-  ],
-  resolve: {
-    alias: {
-      '@': path.resolve(__dirname, './src'),
-    },
-  },
-  server: {
-    port: 5173,
-    host: true,
-    proxy: {
-      '/api': {
-        target:process.env.VITE_API_URL || 'http://localhost:3000',
-        changeOrigin: true,
+export default defineConfig(({ mode }) => {
+  const env = loadEnv(mode, process.cwd(), '');
+  
+  return {
+    plugins: [
+      basicSsl(),
+      react({
+        babel: {
+          plugins: ['babel-plugin-react-compiler'],
+        },
+      })
+    ],
+    resolve: {
+      alias: {
+        '@': path.resolve(__dirname, './src'),
       },
     },
-  },
+    server: {
+      port: 5173,
+      host: true,
+      proxy: {
+        '/api': {
+          target: env.VITE_API_URL || 'http://localhost:3000',
+          changeOrigin: true,
+        },
+      },
+    },
+  };
 })
